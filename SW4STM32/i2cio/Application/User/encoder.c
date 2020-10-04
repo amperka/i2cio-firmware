823f776057f1e6f8201075c5e6a685e0180e1f14
Yury Botov <yury.botov@gmail.com>
2020-10-04 17:54:28 +0300

    replace 2 steps algorythm to 4 steps

--- SW4STM32/i2cio/Application/User/encoder.c
+++ SW4STM32/i2cio/Application/User/encoder.c
@@ -12,9 +12,9 @@ typedef struct
   bool enabled;
   uint8_t pinA;
   uint8_t pinB;
-  bool lastStateA;
-  bool lastStateB;
   int8_t unreadedValue;
+  uint8_t stateA; //
+  uint8_t stateB; // эти переменные используются в качестве 4 битного буфера предыдущих состояний
 } Encoder_Type;
 
 static Encoder_Type encoders[MAX_ENCODER_COUNT];
@@ -38,8 +38,8 @@ void setEncoderPins(uint8_t encoder, uint8_t pinA, uint8_t pinB)
 			encoders[encoder].enabled = true;
 			encoders[encoder].pinA = pinA;
 			encoders[encoder].pinB = pinB;
-			encoders[encoder].lastStateA = pinState(pinA);
-			encoders[encoder].lastStateB = pinState(pinB);
+			encoders[encoder].stateA = 0;
+			encoders[encoder].stateB = 0;
 			encoders[encoder].unreadedValue = 0;
 		}
 	}
@@ -61,21 +61,19 @@ static inline bool detect(bool x1, bool x2, bool y1, bool y2)
 	return (x2 ^ y1) & ~(x1 ^ y2);
 }
 
-void encoderCapture(){
+void encoderCapture() {
 	for (uint8_t i = 0; i < MAX_ENCODER_COUNT; ++i){
 		if (encoders[i].enabled){
 			bool stateA = pinState(encoders[i].pinA);
 			bool stateB = pinState(encoders[i].pinB);
-			bool _stateA = encoders[i].lastStateA;
-			bool _stateB = encoders[i].lastStateB;
-			
-			if (detect(_stateA, _stateB, stateA, stateB))
-				encoders[i].unreadedValue++;
-			else if (detect(_stateB, _stateA, stateB, stateA))
-				encoders[i].unreadedValue--;
-
-			encoders[i].lastStateA = stateA;
-			encoders[i].lastStateB = stateB;
+			if(((encoders[i].stateA & 1) != (int)stateA) || ((encoders[i].stateB & 1) != (int)stateB)) { // если состояние изменилось
+				encoders[i].stateA = ((encoders[i].stateA << 1) | (int)stateA) & 0xf; // записать новое состояние в буфер состояний
+				encoders[i].stateB = ((encoders[i].stateB << 1) | (int)stateB) & 0xf; //
+				if(encoders[i].stateA == 6 && encoders[i].stateB == 3) // цифры 3 и 6 это битовые шаблоны соответствующих состояний
+					encoders[i].unreadedValue++;
+				if(encoders[i].stateA == 3 && encoders[i].stateB == 6)
+					encoders[i].unreadedValue--;
+			}
 		}
 	}
 }
